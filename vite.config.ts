0c63a8dbf2ef5efacd0c29483dc6f698406c823d
fellipe.marra <fellipe.marra@sankhya.com.br>
2025-07-30 14:25:10 -0300

    Ajustes deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    include: ['buffer', 'process'],
-    exclude: ['lucide-react'],
-  },
-  define: {
-    global: 'window',
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
-  },
-});
+})
